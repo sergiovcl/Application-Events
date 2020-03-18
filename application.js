7d021bb2b3f0a6c94cb8c34a4eabb5aeb034235f
sergiovcl <sergiovinicius.lima98@gmail.com>
2020-03-18 09:21:36 -0300

    primeiro commit

---
+++ application.js
@@ -0,0 +1,15 @@
+document.getElementById("btnTriggerClick").addEventListener("click", onClickBtn);
+
+function onClickBtn() {
+    document.getElementById("msg").innerHTML = "Você clicou no botão!";
+}
+
+document.getElementById("color").onmouseover = function () { mouseOver() };
+function mouseOver() {
+    document.getElementById("color").style.color = "red";
+}
+
+document.getElementById("color").onmouseout = function () { mouseOut() };
+function mouseOut() {
+    document.getElementById("color").style.color = "black";
+}
