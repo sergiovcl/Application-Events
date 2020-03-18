766fb1b86170bdb5828a3a021f3bfb3af911f7df
sergiovcl <sergiovinicius.lima98@gmail.com>
2020-03-18 10:11:37 -0300

    inciando pdca

--- application.js
+++ application.js
@@ -13,3 +13,10 @@ document.getElementById("color").onmouseout = function () { mouseOut() };
 function mouseOut() {
     document.getElementById("color").style.color = "black";
 }
+
+
+document.getElementById("clique").addEventListener("click", onClickBtn);
+
+function onClickBtn() {
+    document.getElementById("msgm").innerHTML = "Plano de ação";
+}
