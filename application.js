40318f90211d4ae5badb651e5c1f41907240800f
sergiovcl <sergiovinicius.lima98@gmail.com>
2020-03-18 21:44:53 -0300

    PDCA testes

--- application.js
+++ application.js
@@ -14,20 +14,68 @@ function mouseOver() {
 document.getElementById("color").onmouseout = function () { mouseOut() };
 function mouseOut() {
     document.getElementById("color").style.color = "black";
-}
+};
 
 //PDCA P
-document.getElementById("msgm").onclick = function () { myFunction() };
+document.getElementById("p").onclick = function () { myFunction() };
 
 function myFunction() {
-    document.getElementById("msgm").innerHTML = "Plano de Ação!";
+    document.getElementById("p").innerHTML = "Plano de Ação!";
 }
-document.getElementById("msgm").onmouseover = function () { mouseOver() };
+document.getElementById("p").onmouseover = function () { mouseOver() };
 function mouseOver() {
-    document.getElementById("msgm").style.color = "yellow";
+    document.getElementById("p").style.color = "red";
 }
 
-document.getElementById("msgm").onmouseout = function () { mouseOut() };
+document.getElementById("p").onmouseout = function () { mouseOut() };
 function mouseOut() {
-    document.getElementById("msgm").style.color = "black";
+    document.getElementById("p").style.color = "black";
+};
+
+//PDCA D
+document.getElementById("d").onclick = function () { myFunction() };
+
+function myFunction() {
+    document.getElementById("d").innerHTML = "Execução do plano";
 }
+document.getElementById("d").onmouseover = function () { mouseOver() };
+function mouseOver() {
+    document.getElementById("d").style.color = "yellow";
+}
+
+document.getElementById("d").onmouseout = function () { mouseOut() };
+function mouseOut() {
+    document.getElementById("d").style.color = "black";
+};
+
+//PDCA C
+document.getElementById("c").onclick = function () { myFunction() };
+
+function myFunction() {
+    document.getElementById("c").innerHTML = "Checagem do Projeto";
+}
+document.getElementById("c").onmouseover = function () { mouseOver() };
+function mouseOver() {
+    document.getElementById("c").style.color = "blue";
+}
+
+document.getElementById("c").onmouseout = function () { mouseOut() };
+function mouseOut() {
+    document.getElementById("c").style.color = "black";
+};
+
+//PDCA A
+document.getElementById("a").onclick = function () { myFunction() };
+
+function myFunction() {
+    document.getElementById("a").innerHTML = "Identificação de correções";
+}
+document.getElementById("a").onmouseover = function () { mouseOver() };
+function mouseOver() {
+    document.getElementById("a").style.color = "green";
+}
+
+document.getElementById("a").onmouseout = function () { mouseOut() };
+function mouseOut() {
+    document.getElementById("a").style.color = "black";
+};
